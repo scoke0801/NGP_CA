878898a5c907622f31001b68b856f1e74f783051
LJH <scoke0801@gmail.com>
2020-11-22 01:52:41 +0900

    Update Interface codes
    1. Split files
    2. Define default communication data structs
    3. GameClientThread -> ClientThread
    In this thread, distribute the work accoring to the scene format

--- Server/CAServer/main.cpp
+++ Server/CAServer/main.cpp
@@ -1,60 +1,10 @@
 #include "stdafx.h"
+#include "Datas.h"
+#include "Communicates.h"
 
-#pragma warning(disable  : 4996)    // mbstowcs unsafe###
-// 파일 저장 함수
-void saveFile(string filename, vector<string> fileData);
-
-// 클라가 송신한 상태 정보와 플레이어의 위치 정보를 이용하여 플레이어의 위치정보를 갱신
-void playerUpdate();
-
-// 충돌처리 구현
-void collisionCheck();
-
-enum class Scenes
-{
-	TitleScene = 0,
-	LobbyScene,
-	GameRecordScene,
-
-};
-
-class Point2D
-{
-
-};
-
-DWORD WINAPI GameSceneThread(LPVOID arg) {
-	SOCKET client_sock = (SOCKET)arg;
-	int retVal;
-	SOCKADDR_IN clientAddr;
-	int addrLen;
-	char buf[BUFSIZE + 1];
-
-	// 클라이언트 정보 받기
-	addrLen = sizeof(clientAddr);
-	getpeername(client_sock, (SOCKADDR*)&clientAddr, &addrLen);
-
-	// 송신할 데이터 구조체
-	Point2D Player_position;			// 플레이어의 위치정보
-	int Player_waterRange;				// 플레이어의 공격가능범위
-	int Player_speed;					// 플레이어의 속도
-	int Player_state;					// 플레이어의 상태
-	int mapData[width][height];			// 맵정보
-	bool isGameEnd;						// 게임이 끝났는지 여부
-
-	// 구현 내용..
-
-
-	// closesocket()
-	closesocket(client_sock);
-	cout << "[TCP 서버] 클라이언트 종료 : IP 주소 = " << inet_ntoa(clientAddr.sin_addr)
-		<< ", 포트 번호 = " << ntohs(clientAddr.sin_port) << endl;
-
-
-	return 0;
-}
-
+ 
 int main(int argc, char* argv[]) {
+#pragma region ForDebugHide
 	int retVal;
 
 	// 윈속 초기화
@@ -78,14 +28,13 @@ int main(int argc, char* argv[]) {
 	// listen() 
 	retVal = listen(listen_sock, SOMAXCONN);
 	if (retVal == SOCKET_ERROR) { err_quit("listen()"); }
-
+#pragma endregion
 	// 데이터 통신에 사용할 변수
 	SOCKET client_sock;
 	SOCKADDR_IN clientAddr;
 	int addrLen;
 
 	HANDLE hThread;
-
 	while (1) {
 		// accept()
 		addrLen = sizeof(clientAddr);
@@ -100,8 +49,9 @@ int main(int argc, char* argv[]) {
 			<< ", 포트 번호 = " << ntohs(clientAddr.sin_port) << endl;
 
 		// 스레드 생성
-		hThread = CreateThread(NULL, 0, GameSceneThread, (LPVOID)client_sock, 0, NULL);
-
+		hThread = CreateThread(NULL, 0,
+			ClientThread, (LPVOID)client_sock, 
+			0, NULL);
 
 		if (hThread == NULL) { closesocket(client_sock); }
 		else { CloseHandle(hThread); }
@@ -110,75 +60,6 @@ int main(int argc, char* argv[]) {
 	// closesocket()
 	closesocket(listen_sock);
 
-
 	WSACleanup();
 	return 0;
-}
-void charToWchar(const char* msg, wchar_t* out)
-{
-	mbstowcs(out, msg, strlen(msg) + 1);//Plus null
-}
-void err_quit(const char* msg)
-{
-	wchar_t wMsg[20];
-	LPVOID lpMsgBuf;
-	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-		NULL, WSAGetLastError(),
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPTSTR)&lpMsgBuf, 0, NULL);
-
-	charToWchar(msg, wMsg);
-	MessageBox(NULL, (LPCTSTR)lpMsgBuf, wMsg, MB_ICONERROR);
-	LocalFree(lpMsgBuf);
-	exit(1);
-
-}
-
-void err_display(const char* msg)
-{
-
-	LPVOID lpMsgBuf;
-	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-		NULL, WSAGetLastError(),
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPTSTR)&lpMsgBuf, 0, NULL);
-	printf("[%s] %s", msg, (char*)lpMsgBuf);
-	LocalFree(lpMsgBuf);
-
-}
-
-int recvn(SOCKET s, char* buf, int len, int flags)
-{
-	int received;
-	char* ptr = buf;
-	int left = len;
-
-	while (left > 0) {
-		received = recv(s, ptr, left, flags);
-
-		if (received == SOCKET_ERROR) { return SOCKET_ERROR; }
-		else if (received == 0) { break; }
-
-		left -= received;
-		ptr += received;
-	}
-
-	return (len - left);
-}
-
-void saveFile(string filename, vector<string> fileData)
-{
-
-}
-
-void playerUpdate()
-{
-
-}
-
-void collisionCheck()
-{
-
 }
--- Server/CAServer/stdafx.h
+++ Server/CAServer/stdafx.h
@@ -19,12 +19,4 @@ using namespace std;
 #define width 480 
 #define height 960
 
-// 소켓 함수 오류 출력 후 종료
-void err_quit(const char* msg);
-
-// 소켓 함수 오류 출력
-void err_display(const char* msg);
-
-// recv를 이용한 사용자 정의함수
-int recvn(SOCKET s, char* buf, int len, int flags);
 
